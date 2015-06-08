34ed6136fc2f7ecc95ec96f450f2f14a47b5a1cf
Prabhash Rathore <prabhashrathore@gmail.com>
2015-06-07 22:45:49 -0700

    Added functionality to ten next activity on click of submit

--- MyFirstApp/app/src/main/java/com/mycompany/myfirstapp/MyActivity.java
+++ MyFirstApp/app/src/main/java/com/mycompany/myfirstapp/MyActivity.java
@@ -1,12 +1,17 @@
 package com.mycompany.myfirstapp;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
 
 public class MyActivity extends ActionBarActivity {
+
+    public final static String EXTRA_MESSAGE = "com.mycompany.myfirstapp.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,4 +41,20 @@ public class MyActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    /**
+     * Called when user clicks on send button
+     */
+    public void sendMessage(View view) {
+
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+
+        startActivity(intent);
+
+    }
+
 }
